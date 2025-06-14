8414137e7cfca8b64b9c4c23c158d3a3c1eff7cd
Muruganvc <vcmuruganmca@gmail.com>
2025-06-14 15:42:02 +0530

    dynamic form created

--- src/app/components/settings/new-user/new-user.component.ts
+++ src/app/components/settings/new-user/new-user.component.ts
@@ -1,12 +1,87 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { DynamicFormComponent } from '../../shared/dynamic-form/dynamic-form.component';
 
 @Component({
   selector: 'app-new-user',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, DynamicFormComponent],
   templateUrl: './new-user.component.html',
   styleUrl: './new-user.component.scss'
 })
 export class NewUserComponent {
+  form = new FormGroup({
+    name: new FormControl(''),
+    gender: new FormControl(''),
+    dob: new FormControl(null),
+    agree: new FormControl(false),
+    role: new FormControl(''),
+    subscribe: new FormControl(true)
+  });
+
+/*
+  
+  fields = [
+    
+
+    // Row 1 – 2 columns
+    { type: 'input', name: 'firstName', label: 'First Name', colSpan: 6 },
+    { type: 'input', name: 'lastName', label: 'Last Name', colSpan: 6 },
+    // Row 2 – full width
+    { type: 'input', name: 'userName', label: 'User Name', colSpan: 12 },
+    // Row 2 – 2 columns
+    { type: 'input', name: 'email', label: 'Email', colSpan: 6 },
+    { type: 'input', name: 'phone', label: 'Phone', colSpan: 6 },
+
+    // Row 3 – 3 columns
+    { type: 'input', name: 'city', label: 'City', colSpan: 4 },
+    { type: 'input', name: 'state', label: 'State', colSpan: 4 },
+    { type: 'input', name: 'zip', label: 'Zip Code', colSpan: 4 }
+  ];
+
+  */
+
+  // fields = [
+  //   { type: 'input', name: 'firstName', label: 'First Name', colSpan: 6 },
+  //   { type: 'input', name: 'lastName', label: 'Last Name', colSpan: 6 },
+  //   { type: 'input', name: 'email', label: 'Email Id' },
+  //   {
+  //     type: 'select', name: 'gender', label: 'Gender', options: [
+  //       { value: 'male', label: 'Male' },
+  //       { value: 'female', label: 'Female' }
+  //     ]
+  //   },
+  //   { type: 'date', name: 'dob', label: 'Date of Birth' },
+  //   {
+  //     type: 'radio', name: 'role', label: 'Role', options: [
+  //       { value: 'admin', label: 'Admin' },
+  //       { value: 'user', label: 'User' }
+  //     ]
+  //   },
+  //   { type: 'toggle', name: 'subscribe', label: 'Subscribe to Newsletter' }
+  // ];
+
+  fields = [
+  { type: 'input', name: 'fullName', label: 'Full Name', colSpan: 12 },
+  { type: 'input', name: 'email', label: 'Email', colSpan: 6 },
+  { type: 'password', name: 'password', label: 'Password', colSpan: 6 },
+  { type: 'input', name: 'city', label: 'City', colSpan: 4 },
+  { type: 'select', name: 'state', label: 'State', colSpan: 4, options: [
+      { label: 'Tamil Nadu', value: 'TN' },
+      { label: 'Kerala', value: 'KL' },
+      { label: 'Karnataka', value: 'KA' },
+    ]
+  },
+  { type: 'input', name: 'zip', label: 'Zip Code', colSpan: 4 },
+  { type: 'checkbox', name: 'subscribe', label: 'Subscribe to newsletter', colSpan: 12 },
+  { type: 'radio', name: 'gender', label: 'Gender', colSpan: 6, options: [
+      { label: 'Male', value: 'male' },
+      { label: 'Female', value: 'female' },
+    ]
+  },
+  { type: 'toggle', name: 'active', label: 'Is Active?', colSpan: 6 },
+  { type: 'date', name: 'dob', label: 'Date of Birth', colSpan: 12 }
+];
+
 
 }
--- src/app/components/shared/dynamic-form/dynamic-form.component.ts
+++ src/app/components/shared/dynamic-form/dynamic-form.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
-
+import { Component, inject, input, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-dynamic-form',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSlideToggleModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './dynamic-form.component.html',
   styleUrl: './dynamic-form.component.scss'
 })
 export class DynamicFormComponent {
+  @Input() formGroup!: FormGroup;
+  @Input() fields: any[] = [];
 
+  form: FormGroup = new FormGroup({});
+
+  private fb = inject(FormBuilder);
+
+  title = input.required<string>();
+
+  ngOnInit() {
+    const group: any = {};
+    for (const field of this.fields) {
+      group[field.name] = new FormControl('');
+    }
+    this.form = this.fb.group(group);
+  }
+
+  submitForm() {
+    console.log(this.form.value);
+  }
 }
